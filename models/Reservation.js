a86f60e07dbc655b7f138378fcaef1af686aa7ce
Ivana <rubicode01@gmail.com>
2022-06-17 15:41:22 +0200

    change models

--- models/Reservation.js
+++ models/Reservation.js
@@ -5,14 +5,22 @@ import Place from "../models/Place.js";
 const { Schema, model } = mongoose;
 
 const reservationSchema = new Schema({
+  // Informationen über den User der buchen will
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
   },
+
+  // Sitzplatz den sich der User ausgesucht hat
   place: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Place",
   },
+
+  // zeitraum (von /bis) der User den Platz in anspuch nehmen will
+  from: { type: Date },
+  to: { type: Date },
+
   // user: [{ type: Schema.Types.ObjectId, ref: "User" }],
   // newPlace: [{ type: Schema.Types.ObjectId, ref: "Place" }],
 });
