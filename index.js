52615378f001c69cea2c424ace25d2ea48cd031c
Christian Scholtysik <christianscholtysik@gmail.com>
2022-07-01 11:27:13 +0200

    change multer

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import express from "express";
 import userRouter from "./routes/userRouter.js";
 import placeRouter from "./routes/placeRouter.js";
 import reservationRouter from "./routes/reservationRouter.js";
+// import multer from "multer";
 // import cors from "cors";
 // import placeRouter from "./routes/placeRouter.js";
 
@@ -13,6 +14,18 @@ import reservationRouter from "./routes/reservationRouter.js";
 
 const app = express();
 const port = process.env.PORT || 5000;
+// const multer = require("multer");
+// const path = require ('path')
+// const storage =multer.diskStorage({destination:(req, file, cb)=>{cb(
+//   null, 'images'
+// )}
+// filename: (req, file, cb) =>{
+//   console.log(file)
+//   cb(null, Date.now() + path.extname(file.originalname))
+// }
+//  })
+
+// const upload = multer({storage: })
 
 //Middleware
 // const corsOptions = {
@@ -24,6 +37,14 @@ app.use(express.json());
 // app.use(cors());
 //Router User // Home Router
 app.use("/api/user", userRouter);
+
+//Image Upload
+// app.get("/upload", (req, res) => {
+//   res.render("upload");
+// });
+// app.post("/upload",upload.single('image'), (req, res) => {
+//   res.send("Image Uploaded");
+// });
 
 //Router Place
 app.use("/api/place", placeRouter);
